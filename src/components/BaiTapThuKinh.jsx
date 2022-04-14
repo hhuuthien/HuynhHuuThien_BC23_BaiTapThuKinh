7888a174821d13e28a62fdb91e6325b85602c745
Huỳnh Hữu Thiện <hhuuthien@gmail.com>
2022-04-14 13:31:46 +0700

    binding du lieu json

--- src/components/BaiTapThuKinh.jsx
+++ src/components/BaiTapThuKinh.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from "react";
 import style from "../assets/style/style.module.css";
+import data from "../data/dataGlasses.json";
 
 export default class BaiTapThuKinh extends Component {
+  renderGlassesList() {
+    // console.log(data);
+    return data.map((item, index) => {
+      return <img src={item.url.replace("./glassesImage", "./img/glassesImage")} alt={item.name} key={index} className={style.glasses} />;
+    });
+  }
+
   render() {
     return (
       <div className={style.app}>
@@ -15,6 +23,7 @@ export default class BaiTapThuKinh extends Component {
               <p className={style.description}>Light pink square lenses define these sunglasses, ending with amother of pearl effect tip.</p>
             </div>
           </div>
+          <div className={style.glassesList}>{this.renderGlassesList()}</div>
         </div>
       </div>
     );
